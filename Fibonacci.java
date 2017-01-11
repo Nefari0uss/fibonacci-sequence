64cb8da9a5ad44dfcbc3814821fade9c7251023b
Nefari0uss <nnefari0uss@gmail.com>
2017-01-10 20:12:39 -0500

    fix indentation

--- Fibonacci.java
+++ Fibonacci.java
@@ -18,66 +18,66 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
-    public Fibonacci() {}
+	public Fibonacci() {}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
 		int n = getInput();
 		calculateSequence(n);
 
-    } 
+	} 
 
 	private static int getInput() {
 
 		int n;
 		Scanner input = new Scanner(System.in);
-        System.out.println("Generate Fibonacci sequence till until what number? (Fib terms will be less tha nthis number.)");
-        String num = input.nextLine();
-        
-        // Handle input greater than Java's MAX_INT.
-        try {
-            n = Integer.parseInt(num);
-        }
-        catch (NumberFormatException e) {
-            System.out.println("n is too lage. Setting n to be the largest possible int value.");
-            n = Integer.MAX_VALUE;
-            //System.out.println(n);
-        }
-        input.close();
-        return n;
+		System.out.println("Generate Fibonacci sequence till until what number? (Fib terms will be less tha nthis number.)");
+		String num = input.nextLine();
+
+		// Handle input greater than Java's MAX_INT.
+		try {
+			n = Integer.parseInt(num);
+		}
+		catch (NumberFormatException e) {
+			System.out.println("n is too lage. Setting n to be the largest possible int value.");
+			n = Integer.MAX_VALUE;
+			//System.out.println(n);
+		}
+		input.close();
+		return n;
 	}
 
-    private static void calculateSequence (int n)  {
+	private static void calculateSequence (int n)  {
 
-        int a = 0;
-        int b = 1;
-        int c = 1;
+		int a = 0;
+		int b = 1;
+		int c = 1;
 
-        System.out.println("\nF(0): " + a);
-        System.out.println("F(1): " + b);
-        System.out.println("F(2): " + c);        
-        
-        //System.out.println("Generating terms in the sequence.");
-        int i = 3;
-        a = b + c;
-        
-        // Loop until the last two terms combined are greater than n.
-        while (a < n) {
-            System.out.println("F(" + i + "): " + a);
-            b = c;
-            c = a;
-            i++;
+		System.out.println("\nF(0): " + a);
+		System.out.println("F(1): " + b);
+		System.out.println("F(2): " + c);        
 
-            // Handle a > MAX_INT.
-            if ((long)b + c > Integer.MAX_VALUE) {
-                a = Integer.MAX_VALUE;
-            } else {
-                a = b + c;
-            }
-        } // end while
-            
-        System.out.println("\nNo more terms in the sequence.");
-        System.out.println("Generated " + i + " terms.");
-    } 
+		//System.out.println("Generating terms in the sequence.");
+		int i = 3;
+		a = b + c;
+
+		// Loop until the last two terms combined are greater than n.
+		while (a < n) {
+			System.out.println("F(" + i + "): " + a);
+			b = c;
+			c = a;
+			i++;
+
+			// Handle a > MAX_INT.
+			if ((long)b + c > Integer.MAX_VALUE) {
+				a = Integer.MAX_VALUE;
+			} else {
+				a = b + c;
+			}
+		} // end while
+
+		System.out.println("\nNo more terms in the sequence.");
+		System.out.println("Generated " + i + " terms.");
+	} 
 
 } 
